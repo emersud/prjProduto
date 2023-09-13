db07e7fe3aa39727219b90113b1cb8da7d503432
emersud <emersud@gmail.com>
2023-09-13 12:32:59 -0300

    first commit

---
+++ src/main/java/com/sesisenai/prjProduto/PrjProdutoApplication.java
@@ -0,0 +1,13 @@
+package com.sesisenai.prjProduto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjProdutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrjProdutoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sesisenai/prjProduto/PrjProdutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sesisenai.prjProduto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjProdutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
